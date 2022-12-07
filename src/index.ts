c975545341cb49815bea2339dc6298dc6a4aac45
Michael Oppitz <michael@shapediver.com>
2022-12-07 14:24:07 +0100

    episode 2

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,253 @@
-import { 
+import {
   createSession,
-  createViewport, 
-  BUSY_MODE_DISPLAY, 
-  FLAG_TYPE, 
-  SPINNER_POSITIONING, 
-  VISIBILITY_MODE 
+  createViewport,
+  IParameterApi,
+  ISessionApi,
+  PARAMETER_TYPE
 } from "@shapediver/viewer";
+
+const createParameterDiv = (parameterObject: IParameterApi<any>, parentDiv: HTMLDivElement) => {
+  // create a div to store the parameter data
+  const parameterDiv = document.createElement('div') as HTMLDivElement;
+  parentDiv.appendChild(parameterDiv)
+
+  // create a label with the parameter name
+  const labelElement = document.createElement('label') as HTMLLabelElement;
+  parameterDiv.appendChild(labelElement)
+  labelElement.innerText = parameterObject.displayname || parameterObject.name;
+
+  return parameterDiv;
+}
+
+
+const createBooleanParameterElement = (session: ISessionApi, parameterObject: IParameterApi<boolean>, parentDiv: HTMLDivElement) => {
+  // create a div to store the parameter data
+  const parameterDiv = createParameterDiv(parameterObject, parentDiv);
+
+  // create an input element to manipulate the parameter
+  const inputElement = document.createElement('input') as HTMLInputElement;
+  parameterDiv.appendChild(inputElement)
+  inputElement.type = 'checkbox';
+  inputElement.checked = parameterObject.value === true || parameterObject.value === 'true';
+  inputElement.onchange = async () => {
+    parameterObject.value = inputElement.checked;
+    await session.customize();
+  }
+}
+
+const createColorParameterElement = (session: ISessionApi, parameterObject: IParameterApi<string>, parentDiv: HTMLDivElement) => {
+  // create a div to store the parameter data
+  const parameterDiv = createParameterDiv(parameterObject, parentDiv);
+
+  // create an input element to manipulate the parameter
+  const inputElement = document.createElement('input') as HTMLInputElement;
+  parameterDiv.appendChild(inputElement)
+  inputElement.type = 'color';
+  inputElement.value = parameterObject.value.replace("0x", "#").substring(0, 7);
+  inputElement.onchange = async () => {
+    parameterObject.value = inputElement.value;
+    await session.customize();
+  }
+}
+
+const createStringParameterElement = (session: ISessionApi, parameterObject: IParameterApi<string>, parentDiv: HTMLDivElement) => {
+  // create a div to store the parameter data
+  const parameterDiv = createParameterDiv(parameterObject, parentDiv);
+
+  // create an input element to manipulate the parameter
+  const inputElement = document.createElement('input') as HTMLInputElement;
+  parameterDiv.appendChild(inputElement)
+  inputElement.type = 'text';
+  inputElement.value = parameterObject.value;
+  inputElement.onchange = async () => {
+    parameterObject.value = inputElement.value;
+    await session.customize();
+  }
+}
+
+const createFloatParameterElement = (session: ISessionApi, parameterObject: IParameterApi<number>, parentDiv: HTMLDivElement) => {
+  // create a div to store the parameter data
+  const parameterDiv = createParameterDiv(parameterObject, parentDiv);
+
+  // create an input element to manipulate the parameter
+  const inputElement = document.createElement('input') as HTMLInputElement;
+  parameterDiv.appendChild(inputElement)
+  inputElement.type = 'range';
+  inputElement.value = parameterObject.value + '';
+  inputElement.min = parameterObject.min + '';
+  inputElement.max = parameterObject.max + '';
+  inputElement.step = 1 / Math.pow(10, parameterObject.decimalplaces!) + "";
+
+  inputElement.onchange = async () => {
+    parameterObject.value = inputElement.value;
+    await session.customize();
+  }
+}
+
+const createNumberParameterElement = (session: ISessionApi, parameterObject: IParameterApi<number>, parentDiv: HTMLDivElement) => {
+  // create a div to store the parameter data
+  const parameterDiv = createParameterDiv(parameterObject, parentDiv);
+
+  // create an input element to manipulate the parameter
+  const inputElement = document.createElement('input') as HTMLInputElement;
+  parameterDiv.appendChild(inputElement)
+  inputElement.type = 'range';
+  inputElement.value = parameterObject.value + '';
+  inputElement.min = parameterObject.min + '';
+  inputElement.max = parameterObject.max + '';
+
+  if (parameterObject.type === PARAMETER_TYPE.INT) {
+    inputElement.step = "1";
+  } else if (parameterObject.type === PARAMETER_TYPE.EVEN || parameterObject.type === PARAMETER_TYPE.ODD) {
+    inputElement.step = "2";
+  } else {
+    inputElement.step = 1 / Math.pow(10, parameterObject.decimalplaces!) + "";
+  }
+
+  inputElement.onchange = async () => {
+    parameterObject.value = inputElement.value;
+    await session.customize();
+  }
+}
+
+const createFileParameterElement = (session: ISessionApi, parameterObject: IParameterApi<File | Blob | String>, parentDiv: HTMLDivElement) => {
+  // create a div to store the parameter data
+  const parameterDiv = createParameterDiv(parameterObject, parentDiv);
+
+  // create an input element to manipulate the parameter
+  const inputElement = document.createElement('input') as HTMLInputElement;
+  parameterDiv.appendChild(inputElement)
+  inputElement.type = 'file';
+  inputElement.accept = parameterObject.format.join(',');
+
+  inputElement.onchange = async () => {
+    if (!inputElement.files || !inputElement.files[0]) return;
+    parameterObject.value = inputElement.files[0];
+    await session.customize();
+  }
+}
+
+const createStringListParameterElement = (session: ISessionApi, parameterObject: IParameterApi<string>, parentDiv: HTMLDivElement) => {
+  // create a div to store the parameter data
+  const parameterDiv = createParameterDiv(parameterObject, parentDiv);
+
+  // create a select element to manipulate the parameter
+  const selectElement = document.createElement('select') as HTMLSelectElement;
+  parameterDiv.appendChild(selectElement);
+
+  for (let i = 0; i < parameterObject.choices.length; i++) {
+    const option = document.createElement("option") as HTMLOptionElement;
+    option.value = i + "";
+    option.innerText = parameterObject.choices[i];
+    selectElement.appendChild(option);
+  }
+
+  selectElement.onchange = async () => {
+    parameterObject.value = selectElement.value;
+    await session.customize();
+  }
+}
 
 // we put all of our code here in an IIFE to allow the "await" statement of promises
 (async () => {
-
-  // we read out the canvas element of the index.html file
-  const canvasElement = document.getElementById("canvas") as HTMLCanvasElement;
-
   // we create a viewport with the canvas element we just read out
-  // additionally we adjust some optional properties to change the style of the viewport
   const viewport = await createViewport({
-    canvas: canvasElement,
-    visibility: VISIBILITY_MODE.MANUAL,
-    branding: {
-      logo: "https://viewer.shapediver.com/v3/youtube/video1/catAstronaut.png",
-      backgroundColor: "#008800",
-      spinnerPositioning: SPINNER_POSITIONING.BOTTOM_LEFT,
-      busyModeSpinner: "https://viewer.shapediver.com/v3/youtube/video1/subscribe.gif",
-      busyModeDisplay: BUSY_MODE_DISPLAY.SPINNER
-    },
-    id: "YouTubeViewport1"
+    canvas: document.getElementById("canvas") as HTMLCanvasElement
   })
 
   // we create a session with the ticket and modelViewUrl of a model on the ShapeDiver platform
-  // we additionally adjust some optional properties
   const session = await createSession({
-    ticket: "90377527eeeafdd35ed9c698d21b100d4c7388444aac60838be7ff176eed6b979a8c92632db8f3e96d76c913b5ee0c4ea4bec0908aed7b0fd358b690dd3dc23b39d86476293d56fab2f1ed07615095b459ee9c9e68f9718488a22111dbfb54e0d3f6b315174a46-15abf4140442e14d3fb0095c438c5939",
-    modelViewUrl: "https://sdeuc1.eu-central-1.shapediver.com",
-    initialParameterValues: {
-      "de76cade-0cea-47b1-879e-1a0b717910e1": "2"
-    },
-    id: "YouTubeSession1"
+    ticket: "49c24476e7ab6d9a883d9a92170085a725c7c411778d4fe4776261d6a64dc90ba44ae5178f6254936ce3fdd6edfcab58cf8b2bd55ed83919d7113864d81654ca4c82d5656f835d4d9df978b8c66d45eb73dca020079863fd74f0eee41d26bcd5d6094cf90bc235-7e055d8491f882af523591404f2c8ed7",
+    modelViewUrl: "https://sdr7euc1.eu-central-1.shapediver.com"
   })
 
-  // once everything is set up, we show the viewport (when setting the visibility of the viewport to SESSION, this call is not needed)
-  viewport.show = true;
+  const menuDiv = document.getElementById('menu') as HTMLDivElement;
 
-  // we add a flag to always show the busy mode
-  // this is just done to show the gif
-  viewport.addFlag(FLAG_TYPE.BUSY_MODE)
+  // order the parameters via their order property
+  const orderedParameters = Object.values(session.parameters).sort((a: IParameterApi<any>, b: IParameterApi<any>) => a.order - b.order)
+
+  for (let i = 0; i < orderedParameters.length; i++) {
+    const parameterObject = orderedParameters[i];
+
+    // parameters can be hidden via the platform
+    if (parameterObject.hidden === true) continue;
+
+    // if a parameter has a group, we want to display it within that group
+    let parentDiv: HTMLDivElement = menuDiv;
+    if (parameterObject.group !== undefined) {
+      // we search if there already exists an element with the group id, if not, we create one
+      let parameterGroupDiv = document.getElementById(parameterObject.group.id) as HTMLDivElement | null;
+      if (!parameterGroupDiv) {
+        // we create a new div for this parameter group
+        parameterGroupDiv = document.createElement('div') as HTMLDivElement;
+        parameterGroupDiv.id = parameterObject.group.id;
+        menuDiv.appendChild(parameterGroupDiv);
+
+        // create a label for this group
+        const labelElement = document.createElement('label') as HTMLLabelElement;
+        parameterGroupDiv.appendChild(labelElement);
+        labelElement.innerText = parameterObject.group.name;
+        labelElement.style.fontWeight = "bold";
+      }
+      // if there is a group we add the parameter elements to it
+      parentDiv = parameterGroupDiv;
+    }
+
+    // depending on the type of parameter, we create the correct input elements in the menu
+    switch (parameterObject.type) {
+      case PARAMETER_TYPE.BOOL:
+        createBooleanParameterElement(session, parameterObject, parentDiv);
+        break;
+      case PARAMETER_TYPE.FLOAT:
+      case PARAMETER_TYPE.ODD:
+      case PARAMETER_TYPE.EVEN:
+      case PARAMETER_TYPE.INT:
+        createNumberParameterElement(session, parameterObject, parentDiv);
+        break;
+      case PARAMETER_TYPE.COLOR:
+        createColorParameterElement(session, parameterObject, parentDiv);
+        break;
+      case PARAMETER_TYPE.STRING:
+        createStringParameterElement(session, parameterObject, parentDiv);
+        break;
+      case PARAMETER_TYPE.FILE:
+        createFileParameterElement(session, parameterObject, parentDiv);
+        break;
+      case PARAMETER_TYPE.STRINGLIST:
+        createStringListParameterElement(session, parameterObject, parentDiv);
+        break;
+      default:
+        console.log(parameterObject.name, parameterObject.type)
+    }
+  }
+
+
+
+
+  const lengthParameter = session.getParameterByName('Length')[0];
+  lengthParameter.value = 4;
+  await session.customize();
+
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
+  lengthParameter.resetToDefaultValue();
+  await session.customize();
+
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
+  lengthParameter.value = 6;
+  await session.customize();
+
+  lengthParameter.value = 3;
+  lengthParameter.resetToSessionValue();
+  console.log("Reset to session value: ", lengthParameter.value);
+
+  try {
+    lengthParameter.isValid("Clearly not a number");
+  } catch (e) {
+    console.log(e)
+  }
+
+  console.log("Stringified value: ", lengthParameter.stringify());
+
 })();
